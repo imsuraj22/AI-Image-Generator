1e4fc2f701f6c6cd3108de88e0a019d9f0fb2b7c
Suraj <surajgaikwad9881313856@gmail.com>
2024-06-12 22:24:43 +0530

    working on UI

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = (labelName,type,name,placeholder,value,handleChange,isSurpriseMe,handleSurpriseMe) => {
+const FormField = ({labelName,type,name,placeholder,value,handleChange,isSurpriseMe,handleSurpriseMe}) => {
   return (
     <div>
       <div className='flex items-center gap-2 mb-2'>
@@ -9,7 +9,30 @@ const FormField = (labelName,type,name,placeholder,value,handleChange,isSurprise
       >
         {labelName}
       </label>
+      {isSurpriseMe && (
+        <button
+        type="button"
+        onClick={handleSurpriseMe}
+        className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 
+        rounded-[5px] text-black"
+        >
+          Surpirse Me
+
+        </button>
+      )}
+
       </div>
+      <input
+      type={type}
+      id={name}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onchange}
+      required
+      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3 '
+      />
+      
     </div>
   )
 }
