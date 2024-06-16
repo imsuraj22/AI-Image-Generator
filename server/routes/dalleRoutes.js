46d595b39907a9e188ee2dc32cc5b1e201bccd6a
Suraj <surajgaikwad9881313856@gmail.com>
2024-06-16 12:41:36 +0530

    done with UI creating server side

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import { OpenAI } from "openai";
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+
+
+
+dotenv.config();
+const router=express.Router();
+
+router.route('/').get((req,res)=>{
+    res.send('Hello from DALL-E');
+});
+
+router.route('/').post(async (req,res)=>{
+    try {
+        const {prompt}=req.body;
+
+        const aiResponse = await openai.createImage({
+            model: 'dall-e-3',
+            prompt: 'a white siamese cat',
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+            });
+
+        // const aiResponse=await openai.createImage({
+        //     prompt,
+        //     n:1,
+        //     size:'1024x1024',
+        //     response_format:'b64_json',
+        // });
+
+        const image=aiResponse.data.data[0].b64_json;
+        res.status(200).json({photo:image});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message);
+    }
+})
+
+
+export default router;
